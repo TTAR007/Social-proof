9ca9b3eda7c49bc313cbb391303b993b64ef5dd6
TTAR007 <tar127290@gmail.com>
2025-06-20 00:19:06 +0700

    change path of scss file

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
 function buildStyles() {
-    return src('my_css/**/*.scss')
+    return src('my_scss/**/*.scss')
     .pipe(sass())
     .pipe(dest('css'))
 }
 
 function watchTask() {
-    watch(['my_css/**/*.scss'], buildStyles)
+    watch(['my_scss/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
